1eb8daae7d584e6ec4ceb9e196816a609956259a
Keith Elder <[enteremail]>
2013-09-02 08:05:25 -0500

    added openssh to powershell path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,8 +5,9 @@ Set-Alias vi $VIMPATH
 Set-Alias vim $VIMPATH
 
 
-ssh-add
 
 # Load posh-git example profile
 . 'C:\tools\poshgit\dahlbyk-posh-git-8aecd99\profile.example.ps1'
 
+# Add OpenSSH to Path
+#env:Path += ";C:\Program Files (x86)\OpenSSH for Windows\bin"
